b6d85de70e45d7035176fd31d16d9180f3ae46a9
hmayenve <henry@amida.com>
2019-08-27 12:42:19 -0400

    test page update

--- src/components/TestPage.jsx
+++ src/components/TestPage.jsx
@@ -1,20 +1,84 @@
-import React from 'react'
-
+import React, { Component } from 'react'
+import config from 'config/configFromCookie'
+import { maxHeaderSize } from 'http';
+const { apiURL } = config
 /*
  * TestPage: React frontend for --test functionality.
  *
  *
  *
 */
-function TestPage () {
-  return (
-    <div className='app-page'>
-      <h2>Test a Mapping</h2>
-      <p>
-        Please enter a valid DBQ number below, then choose the mapping file you want to test.
-      </p>
-    </div>
-  )
+class TestPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dbqnum: '',
+      uploadInput: '',
+      results: '',
+      isTesting: false,
+    };
+
+    this.handleUpload = this.handleUpload.bind(this);
+  }
+  resetTest = () => {
+    this.setState({
+      dbqnum: '',
+      uploadInput: '',
+      results: '',
+      isTesting: false,
+    })
+  }
+
+  handleUpload(ev) {
+    ev.preventDefault();
+
+    const data = new FormData();
+    data.append('file', this.uploadInput.files[0]);
+    data.append('dbqnum', this.dbqnum.value);
+    fetch(apiURL+'test_dbq', {
+      method: 'POST',
+      body: data,
+    }).then(res => res.text()).then(text => this.setState({ results: text}))
+    this.setState({ isTesting: true })
+    this.resetForm()
+  }
+  render() {
+    let downloadMsg
+    if (this.state.isTesting) {
+      downloadMsg = 'Testing mapping...'
+    }
+    else {
+      downloadMsg = ''
+    }
+    return (
+        <div className='app-page' id='test-page-div'>
+            <h2>Testing a Mapping</h2>
+            Please enter a DBQ number corresponding to the mapping file you would like to test.
+            <form onSubmit={this.handleUpload}>
+                <br></br>
+                <div className="field">
+                    <label className="label">DBQ Number:</label>
+                    <div className="control">
+                        <input ref={(ref) => { this.dbqnum = ref; }} type="text"/>
+                    </div>
+                    <br></br>
+                    <label className="label">Upload Mapping File:</label>
+                    <div className="control">
+                        <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+                    </div>
+                </div>
+                <br></br>
+                <input type="submit" value="Begin Test"/>
+                <div className="runtime-msg">
+                  {downloadMsg}
+                </div>
+                <textarea cols="70" rows="15" value={this.state.results}/>
+                {this.setState({ isTesting: false })}
+            </form>
+        </div>
+    );
+  }
 }
 
 export default TestPage
